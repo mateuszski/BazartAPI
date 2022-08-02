06fb52b24068f1ccd975016b6fb4d934e6024f0f
RogerWitkowski <witkowski.roger@gmail.com>
2022-08-02 11:49:56 +0200

    added more projects to solution, dbContent added, created models(product,category)

---
+++ Bazart.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bazart.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Bazart.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product>
+
+    }
+}
---
+++ Bazart.Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazart.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(300)]
+        [Required]
+        public string Description { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        //[Required]
+        public decimal? Price { get; set; }
+        //[Required]
+        public int Quantity { get; set; }
+        [Required]
+        public bool isForSale { get; set; }
+        [Required]
+        [Display(Name = "Art Picture")]
+        [ValidateNever]
+        public string ImageUrl { get; set; }
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
+        public Category Category { get; set; }
+
+
+    }
+}
